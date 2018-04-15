5520b0e5a79f42c22d6d82fb49c7a14757b036b2
devwz <wellingtonantonio_zaneze@live.com>
2018-04-14 23:38:23 -0300

    Update MainWindow.

--- ArduinoWeather/MainWindow.xaml.cs
+++ ArduinoWeather/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArduinoWeather.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,10 @@ namespace ArduinoWeather
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = platform;
         }
+
+        Platform platform = new Platform("COM3");
+
     }
 }
